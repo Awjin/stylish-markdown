d958c587cb68d7fb63d344fbc8f2276b61189f17
awjin <awjinahn@gmail.com>
2016-10-31 18:41:26 -0400

    Make table of contents and body a little more responsive

--- js/base.js
+++ js/base.js
@@ -1,26 +1,27 @@
 $(document).ready(function() {
-    $("#main-page").css("right", 0);
+    var prev_visibility = $("#table-of-contents").css("visibility");
+    $(window).on("resize orientationchange", function() {
+        if(prev_visibility != $("#table-of-contents").css("visibility")) {
+            switch(prev_visibility) {
+            case "visible":
+                $("#main-page").css("width", "100%");
+                prev_visibility = "hidden";
+                break;
+            case "hidden":
+                $("#main-page").css("width", "75%");
+                prev_visibility = "visible";
+                break;
+            }
+        }
+    });
 
-    if($("#table-of-contents").length > 0) {
-        $(window).on("load resize", function(event) {
-            var window_width = $(window).outerWidth();
-            var contents_left = $("#table-of-contents").offset().left;
-            var main_page_right = window_width - contents_left;
-
-            $("#main-page").css("right", main_page_right);
-        });
-
-        $("#table-of-contents a").click(function(event) {
-            event.preventDefault();
-            this.blur();
-            var href = $(this).attr("href");
-            $("#main-page").animate({"scrollTop": $(href)[0].offsetTop}, 250);
-        });
-    }
+    $("#table-of-contents a").click(function(event) {
+        var href = $(this).attr("href");
+        $("#main-page").animate({"scrollTop": $(href)[0].offsetTop}, 250);
+    });
 
     $("#back-to-top").click(function(event) {
         event.preventDefault();
-        this.blur();
         $("#main-page").animate({"scrollTop": "0"}, 250);
     });
 });
