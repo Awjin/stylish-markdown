d36e811c9955725831539354a6f752eed52bca99
awjin <awjinahn@gmail.com>
2016-11-02 13:52:49 -0400

    Scroll to previous anchor location upon url hash change

--- js/base.js
+++ js/base.js
@@ -1,27 +1,41 @@
 $(document).ready(function() {
-    var prev_visibility = $("#table-of-contents").css("visibility");
+    var curr_visibility, prev_visibility = $("#table-of-contents").css("visibility");
+
     $(window).on("resize orientationchange", function() {
-        if(prev_visibility != $("#table-of-contents").css("visibility")) {
-            switch(prev_visibility) {
+        curr_visibility = $("#table-of-contents").css("visibility");
+
+        if( curr_visibility != prev_visibility) {
+            switch(curr_visibility) {
             case "visible":
-                $("#main-page").css("width", "100%");
-                prev_visibility = "hidden";
+                $("#main-page").css("width", "75%");
                 break;
             case "hidden":
-                $("#main-page").css("width", "75%");
-                prev_visibility = "visible";
+                $("#main-page").css("width", "100%");
                 break;
             }
+
+            prev_visibility = curr_visibility;
         }
     });
 
-    $("#table-of-contents a").click(function(event) {
+    $("#table-of-contents a").click(function() {
         var href = $(this).attr("href");
         $("#main-page").animate({"scrollTop": $(href)[0].offsetTop}, 250);
     });
 
-    $("#back-to-top").click(function(event) {
+    $(window).on("hashchange", function() {
+        var href = window.location.hash;
+
+        if(href) {
+          $("#main-page").animate({"scrollTop": $(href)[0].offsetTop}, 250);
+        } else {
+          $("#main-page").animate({"scrollTop": "0"}, 250);
+        }
+    });
+
+    $("#back-to-top").click(function() {
         event.preventDefault();
-        $("#main-page").animate({"scrollTop": "0"}, 250);
+        this.blur();
+        window.location.hash = "";
     });
 });
