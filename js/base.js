4bac74e54b048e230571c20b3244ea0c71c686d5
awjin <awjinahn@gmail.com>
2016-11-02 18:27:19 -0400

    Clean up, increase efficiency

--- js/base.js
+++ js/base.js
@@ -1,23 +1,4 @@
 $(document).ready(function() {
-    var curr_visibility, prev_visibility = $("#table-of-contents").css("visibility");
-
-    $(window).on("resize orientationchange", function() {
-        curr_visibility = $("#table-of-contents").css("visibility");
-
-        if( curr_visibility != prev_visibility) {
-            switch(curr_visibility) {
-            case "visible":
-                $("#main-page").css("width", "75%");
-                break;
-            case "hidden":
-                $("#main-page").css("width", "100%");
-                break;
-            }
-
-            prev_visibility = curr_visibility;
-        }
-    });
-
     $("#table-of-contents a").click(function() {
         var href = $(this).attr("href");
         $("#main-page").animate({"scrollTop": $(href)[0].offsetTop}, 250);
