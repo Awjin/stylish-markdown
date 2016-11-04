c8f4f73bb9f1a65c73e8b2b96d5b1a60c40fcada
awjin <awjinahn@gmail.com>
2016-11-04 19:05:35 -0400

    Make style more responsive and cross-browser compatible

--- js/base.js
+++ js/base.js
@@ -3,26 +3,27 @@ $(document).ready(function() {
         var href = window.location.hash;
 
         if(href) {
-          $("#main-page").animate({"scrollTop": $(href)[0].offsetTop}, 250);
+          $("html, body").animate({"scrollTop": $(href)[0].offsetTop}, 250);
         } else {
-          $("#main-page").animate({"scrollTop": 0}, 250);
+          $("html, body").animate({"scrollTop": 0}, 250);
         }
     });
 
     $("#table-of-contents-toggle").click(function(event) {
         $(this).blur();
-        $("#table-of-contents").slideToggle(250);
+        $("#table-of-contents").slideToggle(150);
+        $("#table-of-contents").css("display", "inline-block");
     });
 
     $("#table-of-contents a").click(function(event) {
         var href = $(this).attr("href");
-        $("#main-page").animate({"scrollTop": $(href)[0].offsetTop}, 250);
+        $("html, body").animate({"scrollTop": $(href)[0].offsetTop}, 250);
     });
 
     $("#back-to-top").click(function(event) {
         event.preventDefault();
         this.blur();
 
-        $("#main-page").animate({"scrollTop": 0}, 250);
+        $("html, body").animate({"scrollTop": 0}, 250);
     });
 });
